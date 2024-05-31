ad07d7b6051048025377a4266bd43d38b2220986
suryavanshipankaj <suryavanshi2228@gmail.com>
2024-05-31 12:37:23 +0530

    Update visual.py

--- visual.py
+++ visual.py
@@ -3,14 +3,14 @@ from streamlit_echarts import st_echarts
 
 # Sample data
 data = [
-    {"value": 8, "name": 'A'},
-    {"value": 7, "name": 'B'},
-    {"value": 6, "name": 'C'},
-    {"value": 5, "name": 'D'},
-    {"value": 4, "name": 'E'},
-    {"value": 3, "name": 'F'},
-    {"value": 2, "name": 'G'},
-    {"value": 1, "name": 'H'}
+    {"value": 8, "name": 'HR'},
+    {"value": 7, "name": 'Accounts'},
+    {"value": 6, "name": 'Inventory'},
+    {"value": 5, "name": 'Production'},
+    {"value": 4, "name": 'Supply chain'},
+    {"value": 3, "name": 'IT'},
+    {"value": 2, "name": 'DMSD'},
+    {"value": 1, "name": 'IBSD'}
 ]
 
 option = {
@@ -46,4 +46,3 @@ st.title("Nightingale (Rose) Chart Example")
 
 # Display chart in Streamlit
 st_echarts(options=option, height="500px")
-
